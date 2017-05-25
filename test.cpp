b7d41c3926075566b76d9f963c7790185bd2150d
yantong wu <yantong wu>
2017-05-25 15:51:21 +0800

    test.cpp

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+int add(int a,int b)
+{
+	return a+b;
+}
+
+int minus(int a,int b)
+{
+	cout<<"not done"<<endl;
+	return 0xffffffff;
+}
+
+int multi(int a,int b)
+{
+	cout<<"not done"<<endl;
+	return 0xffffffff;
+}
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	
+	system("pause")
+	return 0;
+}
+
+
